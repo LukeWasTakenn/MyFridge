f8b018be4631d6c4074c88ea48cf932e181b5540
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-03-12 21:15:58 +0100

    feat: initial db sql file
    todo: figure out recipe categories

---
+++ recipe.sql
@@ -0,0 +1,59 @@
+CREATE TABLE `accounts`
+(
+    `account_id`   INT AUTO_INCREMENT
+        PRIMARY KEY,
+    `first_name`   VARCHAR(50) NOT NULL,
+    `last_name`    VARCHAR(50) NOT NULL,
+    `email`        VARCHAR(60) NOT NULL,
+    `password`     VARCHAR(60) NOT NULL,
+    `phone_number` VARCHAR(10) NULL,
+    CONSTRAINT `accounts_pk2`
+        UNIQUE (`email`)
+);
+
+CREATE TABLE `ingredients`
+(
+    `ingredient_id` INT AUTO_INCREMENT
+        PRIMARY KEY,
+    `name`          VARCHAR(50)               NOT NULL,
+    `unit`          ENUM ('count', 'ml', 'g') NOT NULL
+);
+
+CREATE TABLE `recipes`
+(
+    `recipe_id`      INT AUTO_INCREMENT
+        PRIMARY KEY,
+    `creator_id`     INT          NOT NULL,
+    `title`          VARCHAR(50)  NOT NULL,
+    `description`    LONGTEXT     NOT NULL,
+    `image`          VARCHAR(255) NOT NULL,
+    `estimate_price` INT(6)       NOT NULL,
+    `estimate_time`  INT(5)       NOT NULL,
+    CONSTRAINT `recipes_accounts_account_id_fk`
+        FOREIGN KEY (`creator_id`) REFERENCES `accounts` (`account_id`)
+);
+
+CREATE TABLE `recipe_ingredients`
+(
+    `recipe_id`     INT     NOT NULL,
+    `ingredient_id` INT     NOT NULL,
+    `amount`        DECIMAL NOT NULL,
+    PRIMARY KEY (`ingredient_id`, `recipe_id`),
+    CONSTRAINT `recipe_ingredients_ingredients_ingredient_id_fk`
+        FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`),
+    CONSTRAINT `recipe_ingredients_recipes_recipe_id_fk`
+        FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`)
+);
+
+CREATE TABLE `recipe_likes`
+(
+    `recipe_id`  INT                              NOT NULL,
+    `account_id` INT                              NOT NULL,
+    `liked_at`   DATE DEFAULT current_timestamp() NOT NULL,
+    PRIMARY KEY (`account_id`, `recipe_id`),
+    CONSTRAINT `recipe_likes_accounts_account_id_fk`
+        FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
+    CONSTRAINT `recipe_likes_recipes_recipe_id_fk`
+        FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`)
+);
+
